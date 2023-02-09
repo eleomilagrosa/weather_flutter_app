5d35bdb949f6c2788d4cf95b74ef1bc0421cd96f
Eleojasmil Milagrosa <eleojasmil.milagrosa@beaconforce.io>
2023-02-09 12:32:27 +0800

    initial commit

---
+++ lib/views/screens/landing/landing_page.v.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LandingPageView extends StatefulWidget {
+  const LandingPageView({Key? key}) : super(key: key);
+
+  @override
+  State<LandingPageView> createState() => _LandingPageViewState();
+}
+
+class _LandingPageViewState extends State<LandingPageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/views/screens/weather/weather_page.v.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherPageView extends StatefulWidget {
+  const WeatherPageView({Key? key}) : super(key: key);
+
+  @override
+  State<WeatherPageView> createState() => _WeatherPageViewState();
+}
+
+class _WeatherPageViewState extends State<WeatherPageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
